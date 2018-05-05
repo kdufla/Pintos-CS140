d38cdf20b9b74a04a83eeba9c10f454d14cfdc8d
kdufla <george.gvelesiani.a@gmail.com>
2018-05-05 09:25:11 +0000

    Start implementing syscalls

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,20 +6,44 @@
 
 static void syscall_handler (struct intr_frame *);
 
+
+static void halt(void);
+static void exit(int status);
+static pid_t exec(const chat *file);
+
+
 void
 syscall_init (void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+	intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
+
+
+static void halt(void)
+{
+	shutdown_power_off(void);
+}
+
+static void exit(int status){
+	thread_current()->status = status;
+	thread_exit();
+}
+
+static pid_t exec(const chat *file){
+	process_execute(file);
+}
+
+
+
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  uint32_t* args = ((uint32_t*) f->esp);
-  printf("System call number: %d\n", args[0]);
-  if (args[0] == SYS_EXIT) {
-    f->eax = args[1];
-    printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
-    thread_exit();
-  }
+	uint32_t* args = ((uint32_t*) f->esp);
+	printf("System call number: %d\n", args[0]);
+	if (args[0] == SYS_EXIT) {
+		f->eax = args[1];
+		printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
+		thread_exit();
+	}
 }
