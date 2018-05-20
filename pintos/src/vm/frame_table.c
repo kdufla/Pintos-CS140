0319b3817b915e48ca174532e5f3d6e53798c331
Babdus <babdusi@gmail.com>
2018-05-20 18:15:46 +0000

    Initialize vm 2

--- pintos/src/vm/frame_table.c
+++ pintos/src/vm/frame_table.c
@@ -10,3 +10,25 @@ frame_table_init ()
 {
 	return;
 }
+
+void *
+get_free_frame ()
+{
+	int i;
+	for (i = 0; i < init_ram_pages; i++)
+		if (is_free_frame (i))
+			return (void *)i;
+	ASSERT (false);
+	return evict ();
+}
+
+bool
+is_free_frame (int index)
+{
+	return (!(frame_table[index].flags & FRAME_OCUPIED));
+}
+
+void *
+evict () {
+	return NULL;
+}
--- pintos/src/vm/frame_table.h
+++ pintos/src/vm/frame_table.h
@@ -4,10 +4,17 @@
 #include "threads/loader.h"
 #include "threads/palloc.h"
 
+enum frame_flags
+  {
+    FRAME_OCUPIED = 001,
+    FRAME_ACCESSED = 002,
+    FRAME_DIRTY = 004
+  };
+
 struct frame
 {
 	struct page *page;
-	int dirty_bits;
+	int flags;                /* first bit - ocupied, second bit - accessed, third bit - dirty */
 };
 
 void frame_table_init (void);
